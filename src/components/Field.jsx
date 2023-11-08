db2590f76f6b03ba905565996d65a64f3ab52e3c
Nancy <ynancy030@gmail.com>
2023-11-08 13:19:26 +0530

    des implemented

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
+import CryptoJS from "crypto-js";
 
 export default function Field() {
 
   const [btnText, setBtnText] = useState("Encode");
-  const handleClick = (text) => {
-    setBtnText(text);
+  const [inputText, setInputText] = useState("");
+  const [outputText, setOutputText] = useState("");
+  const [secretKey, setSecretKey] = useState("");
+  const handleClick = () => {
+    if(btnText === "Encode"){
+      const encryptedText = CryptoJS.DES.encrypt(inputText, secretKey).toString();
+      setOutputText(encryptedText);
+    }
+    else{
+      const decryptedText = CryptoJS.DES.decrypt(inputText, secretKey).toString(CryptoJS.enc.Utf8);
+      setOutputText(decryptedText);
+    }
   }
 
   return (
@@ -16,10 +27,30 @@ export default function Field() {
 
         <div className="border-gray-300 flex justify-between">
           <ul className="flex gap-4">
-            <button className={`hover:bg-gray-200 p-2 rounded-t-lg btnText === 'Encode' ? 'bg-gray-200' : ''`} onClick={() => handleClick("Encode")}>Encryption</button>
-            <button className="hover:bg-gray-200 p-2 rounded-t-lg" onClick={() => handleClick("Decode")}>Decryption</button>
-          </ul> 
-          <input className="m-2 p-2 focus:outline-none" type="text" placeholder="Secret Key" />
+          <button
+              className={`hover:bg-gray-200 p-2 rounded-t-lg ${
+                btnText === "Encode" ? "bg-gray-200" : ""
+              }`}
+              onClick={() => setBtnText("Encode")}
+            >
+              Encryption
+            </button>
+            <button
+              className={`hover:bg-gray-200 p-2 rounded-t-lg ${
+                btnText === "Decode" ? "bg-gray-200" : ""
+              }`}
+              onClick={() => setBtnText("Decode")}
+            >
+              Decryption
+            </button>
+          </ul>
+          <input
+            className="m-2 p-2 focus:outline-none"
+            type="text"
+            placeholder="Secret Key"
+            value={secretKey}
+            onChange={(e) => setSecretKey(e.target.value)} // Added this line
+          />
         </div>
 
         <div className="border-b-2 border-gray-300 bg-gray-200">
@@ -40,16 +71,25 @@ export default function Field() {
               className="h-full w-full p-[6px] focus:outline-none"
               type="text"
               placeholder="Enter your text here..."
-              id=""
-              cols="30"
-              rows="10"
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)} // Added this line
             />
-            <button className="absolute bottom-0 m-2 right-0 pl-4 pr-4 border-2 p-2 bg-gray-700 text-white hover:rounded-xl">
+            <button
+              className="absolute bottom-0 m-2 right-0 pl-4 pr-4 border-2 p-2 bg-gray-700 text-white hover:rounded-xl"
+              onClick={handleClick}
+            >
               {btnText}
             </button>
           </div>
-
-          <div className="w-1/2  h-full border-solid border-gray-400 border-l-[2px]"></div>
+          <div className="w-1/2  h-full border-solid border-gray-400 border-l-[2px] overflow-hidden">
+            <textarea
+              className="h-full w-full p-[6px] focus:outline-none"
+              type="text"
+              placeholder="Result..."
+              value={outputText}
+              readOnly
+            />
+          </div>
         </div>
       </div>
     </div>
