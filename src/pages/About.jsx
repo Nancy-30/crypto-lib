5a2a2ba9d5f8e438cb3a5d914504036e9dc2d6d3
Nancy <ynancy030@gmail.com>
2023-11-21 15:17:42 +0530

    replaced

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,33 @@
 import { useState } from 'react';
-import DES from '../components/DES';
-import { Link } from 'react-router-dom';
-import AES from '../components/AES';
-import BlowFish from '../components/BlowFish';
-import TrippleDES from '../components/TrippleDES';
+import { AiOutlineInfoCircle } from "react-icons/ai"
+import { Link, NavLink } from 'react-router-dom'
+import DES from '../components/descriptions/DES';
+import AES from '../components/descriptions/AES';
+import BlowFish from '../components/descriptions/BlowFish';
+import TrippleDES from '../components/descriptions/TrippleDES';
+import Blockcipher from '../components/descriptions/Blockcipher';
 import cryptify from '../assets/logo.png';
 
 
 export default function About() {
-    const [algo, setAlgo] = useState("");
+    const [algo, setAlgo] = useState("Blockcipher");
     const handleAlgo = (algoName) => {
         setAlgo(algoName);
     }
 
     return (
 
-        <div className='h-[707px] w-full z-10 bg-gradient-to-br from-sky-800 to-emerald-600 text-white p-4 font-mono' >
+        <div className='h-[707px] w-full z-10 bg-gradient-to-br from-sky-800 to-emerald-600 text-white p-4 font-mono select-none' >
             <div>
-            <Link to="/"><img className='h-10' src={cryptify} alt="" /></Link>
+                <div className='h-[50px] w-full text-white pl-[30px] flex justify-between items-center'>
+                    <Link to="/"><img className='h-10' src={cryptify} alt="" /></Link>
+                    <NavLink to="/about" activeClassName="bg-green-400">< AiOutlineInfoCircle className='text-lg mr-[50px] cursor-pointer' /></NavLink>
+                </div>
+
                 <ul className='flex items-center justify-center gap-40'>
+                    <li className='cursor-pointer' onClick={() => handleAlgo("Blockcipher")}>
+                        BlockCipher
+                    </li>
                     <li className='cursor-pointer' onClick={() => handleAlgo("DES")}>
                         DES
                     </li>
@@ -34,10 +43,13 @@ export default function About() {
                 </ul>
             </div>
 
-            {algo === "DES" && <DES />}
-            {algo === "AES" && <AES />}
-            {algo === "BlowFish" && <BlowFish />}
-            {algo === "3DES" && <TrippleDES />}
+            <div className='select-text'>
+                {algo === "Blockcipher" && <Blockcipher />}
+                {algo === "DES" && <DES />}
+                {algo === "AES" && <AES />}
+                {algo === "BlowFish" && <BlowFish />}
+                {algo === "3DES" && <TrippleDES />}
+            </div>
         </div>
 
     );
